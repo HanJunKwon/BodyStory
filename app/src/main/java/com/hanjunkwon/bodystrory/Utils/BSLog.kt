a1af2e51302df0bc6fea031b3f26dd6f99e9b27d
HanjunKwon <skclaqks11@naver.com>
2022-10-23 15:45:08 +0900

    로그 클래스 생성

---
+++ app/src/main/java/com/hanjunkwon/bodystrory/Utils/BSLog.kt
@@ -0,0 +1,23 @@
+package com.hanjunkwon.bodystrory.Utils
+
+import android.util.Log
+
+object BSLog {
+
+    /**
+     * 태그가 없는 개발 로그 출력하는 메소드.
+     * @param message 로그 메세지
+     */
+    fun d(message: String) {
+        Log.d("", message);
+    }
+
+    /**
+     * 태그가 있는 개발 로그 출력하는 메소드.
+     * @param tag 로그 필터링에 사용할 태그
+     * @param message 로그 메세지
+     */
+    fun d(tag: String, message: String) {
+        Log.d(tag, message);
+    }
+}
